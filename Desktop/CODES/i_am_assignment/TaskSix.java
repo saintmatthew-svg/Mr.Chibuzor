821f01987ad6cf8663552df113de4c8951e3a72a
saintmatthew-svg <onyekachimatthew00@gmail.com>
2024-12-07 08:16:39 +0100

    Chibuzor

---
+++ Desktop/CODES/i_am_assignment/TaskSix.java
@@ -0,0 +1,21 @@
+public class TaskSix {
+	public static void main(String... args){
+
+int multiple = 4;
+for(int number = 0; number < 5; number++){
+System.out.println(multiple);
+multiple *= 4;
+
+}
+
+int sample = 8;
+for(int value = 0; value < 5; value++){
+System.out.println(sample);
+sample *= 8;
+
+}
+
+}
+
+
+}
